1fb1af896f056f3209faac8eef4b380298a91e17
nakahashi_h\wsb <nakahashi_h@websandbag.com>
2018-08-17 20:48:09 +0900

    readme追加

--- Assets/Scripts/CustomDepthSourceManager.cs
+++ Assets/Scripts/CustomDepthSourceManager.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Windows.Kinect;
 
 public class CustomDepthSourceManager : MonoBehaviour {
 
-	// Use this for initialization
+    private KinectSensor _Sensor;
+    private DepthFrameReader _Reader;   // 深度センサーのクラスを取得
+    private ushort[] _Data;             // 
+
+
+    public ushort[] GetData()
+    {
+        return _Data;
+    }
+
+
 	void Start () {
-		
+        Debug.Log("start");
+        _Sensor = KinectSensor.GetDefault();
+        if (_Sensor != null)
+        {
+            // センサーを起動する
+            _Reader = _Sensor.DepthFrameSource.OpenReader();
+
+            // ?
+            _Data = new ushort[_Sensor.DepthFrameSource.FrameDescription.LengthInPixels];
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Debug.Log("update");
+        if (_Reader != null)
+        {
+            var frame = _Reader.AcquireLatestFrame();
+            if(frame != null)
+            {
+                frame.CopyFrameDataToArray(_Data);
+                frame.Dispose();
+                frame = null;
+            }
+        }	
 	}
 }
